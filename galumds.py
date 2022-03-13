7007a824c590a293a563ed7d1c82e989399b71ea
fubar2 <ross.lazarus@gmail.com>
2022-03-13 17:24:02 +1100

    Refactored - pulled the generic pg functions out. One specialised function to do everything for the autoflocker.

--- galumds.py
+++ galumds.py
@@ -1,11 +1,15 @@
 """
 Some experimental "Bring code to data" to avoid privacy issues
+
+Couple of generic functions with a specific mds function
+
+Includes fake data generation to test the plot code
 ross lazarus March 12 2022
-pip3 install pandas matplotlib numpy sklearn sqlalchemy psychopg2-binary
+pip3 install pandas matplotlib seaborn numpy sklearn sqlalchemy psychopg2-binary
 need python3-tk if you want to use remote xwindows for images
 so that wants tk
 ah. postgres listens on localhost which is docker
-we live on the host so with bjoern's docker, need to open 5432 or whatever you use on the container
+we live on the host so with bjoern's rna workbench, need to open 5432 or whatever you use on the container
 """
 from datetime import datetime
 import logging
@@ -21,22 +25,23 @@ from sklearn.manifold import MDS
 from sklearn.metrics.pairwise import manhattan_distances, euclidean_distances
 from sqlalchemy import create_engine
 
+NTOOL = 100
+NUSERID = 1000
+NGROUPS = 5
+NCPU = 2 # allowable mds parallel processes, -1 = all !
+
+
 plt.switch_backend('TkAgg') # for x over ssh
 
-NCPU = 2 # this sets the number of allowable mds parallel processes, -1 = all !
-
-def pgjobs(CHUNKSIZE = 1000,
-    POSTGRES_ADDRESS = '127.0.0.1', # Bjoern's docker defaults....
+# override with local values - these are Bjoern's docker defaults.
+def pg_cnx( POSTGRES_ADDRESS = '127.0.0.1',
     POSTGRES_PORT = '5432',
     POSTGRES_USERNAME = 'galaxy',
     POSTGRES_PASSWORD = 'galaxy',
     POSTGRES_DBNAME = 'galaxy',
-    DSTART = '2000-01-01 00:00:01', # forever - might be too big to cope with on main!
-    DFINISH = '2022-06-01 00:00:01'):
+    ):
     """
-    Extract table userid rows by tool invocation counts
-    normalise to total jobs to remove effects of user total volume of analyses
-    User with 10 bwa jobs out of 10 total jobs is a long distance from a user with 10 bwa jobs out of 100000 jobs
+    generic get a connection to postgres
     """
     postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
     password=POSTGRES_PASSWORD,
@@ -44,69 +49,63 @@ def pgjobs(CHUNKSIZE = 1000,
     port=POSTGRES_PORT,
     dbname=POSTGRES_DBNAME))
     cnx = create_engine(postgres_str)
-    squery = '''SELECT user_id, tool_id, COUNT(*) as nruns from job WHERE create_time >= '{}'::timestamp AND create_time < '{}'::timestamp GROUP BY user_id, tool_id  ;'''
-    sql = squery.format(DSTART, DFINISH)
-    log.info('squery=%s' % sql)
+    return cnx
+
+def pg_query(cnx, sql = None, CHUNKSIZE = 1000):
+    """
+    generic run a chunked sql query
+    """
+    log.info('sql=%s' % sql)
     dfs = []
     for chunk in pd.read_sql(sql, con=cnx, chunksize=CHUNKSIZE):
         dfs.append(chunk)
-    jobs = pd.concat(dfs)
+    res = pd.concat(dfs)
+    return res
+
+def mds_query(cnx, DSTART = '2000-01-01 00:00:01',
+                            DFINISH = '2022-06-01 00:00:01'):
+    #forever may be too long on main!!
+    """
+    specific for the autoflocker
+    """
+
+    def plotmds(j):
+        jobs = pd.DataFrame(j)
+        mds = MDS(random_state=0, n_jobs = NCPU)
+        jobs_transform = mds.fit_transform(jobs)
+        size = [5]
+        plt.scatter(jobs_transform[:,0], jobs_transform[:,1], s=size)
+        plt.title('Users in tool usage space')
+        plt.savefig('user_in_toolspace_mds.pdf')
+        # heatmap(mds.dissimilarity_matrix_,j)
+        log.info('stress=%f' % mds.stress_)
+        return mds
+
+    squery = '''SELECT user_id, tool_id, COUNT(*) as nruns from job WHERE create_time >= '{}'::timestamp AND create_time < '{}'::timestamp GROUP BY user_id, tool_id  ;'''
+    sql = squery.format(DSTART, DFINISH)
+    started = time.time()
+    jobs = pg_query(cnx, sql = sql)
+    log.info('Query took %f seconds' % (time.time() - started))
     wjobs = jobs.pivot(index='user_id', columns='tool_id', values='nruns')
     # too hairy to do in SQL !!! Postgres crosstab is horrid - trivial in pandas.
     wjobs = wjobs.fillna(0)
     rjobs = wjobs.div(wjobs.sum(axis=1), axis=0)
     # scale user tool nruns into a fraction of their total work - remove uninteresting total work volume
-    return rjobs
-
-def fakejobs(NTOOL = 100, NUSERID = 1000, NGROUPS = 5):
-    # synthesise NGROUPS obviously different users
-    # to test mds plot code without real data
-    sjob = []
-    for userid in range(NUSERID):
-        srow = []
-        for toolid in range(NTOOL):
-            srow.append(random.randint(0,1000))
-        group = (userid % NGROUPS)
-        for r in range(group, NTOOL, NGROUPS):
-            srow[r] = 5*srow[r] # fake strong group bias
-        # scale so tool frequencies sum to 1
-        fsum = float(sum(srow))
-        nrow = [x/fsum for x in srow]
-        nrow.insert(0,userid)
-        sjob.append(nrow)
-        job = pd.DataFrame(sjob)
-        job = job.drop(job.columns[[0]], axis=1)
-    return job
-
-
-def plotjobs(j):
-    jobs = pd.DataFrame(j)
-    jobarray = euclidean_distances(jobs) # precompute - returns numpy array
-    mds = MDS(random_state=0, dissimilarity = "precomputed", n_jobs = NCPU)
-    jobs_transform = mds.fit_transform(jobarray)
-    size = [5]
-    plt.scatter(jobs_transform[:,0], jobs_transform[:,1], s=size)
-    plt.title('Users in tool usage space')
-    plt.savefig('user_in_toolspace_mds.pdf')
-    log.info('stress=%f' % mds.stress_)
-    return mds
+    mstarted = time.time()
+    nr = len(rjobs)
+    log.info('Retrieving jobs took %f sec and returned %d rows' % (mstarted - started,nr))
+    if nr > 2:
+        mds = plotmds(rjobs)
+        log.info('MDS with %d CPU took %f sec' % (NCPU, time.time() - mstarted))
+    else:
+        log.warning('1 or less rows in query result - check that the time interval is sane?')
 
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger()
-started = time.time()
 log.info('galumds.py starting %s' % datetime.today())
-# adjusted dates for a one month test to start
-jobs = pgjobs(DSTART = '2022-01-01 00:00:01', DFINISH = '2022-01-31 23:59:59')
-# jobs = pgjobs()
-# jobs = fakejobs()
-mstarted = time.time()
-nr = len(jobs)
-log.info('Retrieving jobs took %f sec and returned %d rows' % (mstarted - started,nr))
-if nr > 2:
-    mds = plotjobs(jobs)
-    log.info('MDS with %d CPU took %f sec' % (NCPU, time.time() - mstarted))
-
-else:
-    log.warning('1 or less rows in query result - check that the time interval is sane?')
+cnx = pg_cnx()
+mds_query(cnx, DSTART = '2000-01-01 00:00:01',
+        DFINISH = '2022-06-01 00:00:01')
+        # forever - might be too big to cope with on main!
 log.info('galumds.py finished %s' % datetime.today())
--- plotusermds.py
+++ plotusermds.py
@@ -1,5 +1,8 @@
 """
 Some experimental "Bring code to data" to avoid privacy issues
+
+Couple of generic functions with a specific mds function
+
 Includes fake data generation to test the plot code
 ross lazarus March 12 2022
 pip3 install pandas matplotlib seaborn numpy sklearn sqlalchemy psychopg2-binary
@@ -27,24 +30,20 @@ from sqlalchemy import create_engine
 NTOOL = 100
 NUSERID = 1000
 NGROUPS = 5
-NCPU = 2 # this sets the number of allowable mds parallel processes, -1 = all !
+NCPU = 2 # allowable mds parallel processes, -1 = all !
 
 
 plt.switch_backend('TkAgg') # for x over ssh
 
-
-def pgjobs(CHUNKSIZE = 1000,
-    POSTGRES_ADDRESS = '127.0.0.1', # Bjoern's docker defaults....
+# override with local values - these are Bjoern's docker defaults.
+def pg_cnx( POSTGRES_ADDRESS = '127.0.0.1',
     POSTGRES_PORT = '5432',
     POSTGRES_USERNAME = 'galaxy',
     POSTGRES_PASSWORD = 'galaxy',
     POSTGRES_DBNAME = 'galaxy',
-    DSTART = '2000-01-01 00:00:01', # forever - might be too big to cope with on main!
-    DFINISH = '2022-06-01 00:00:01'):
+    ):
     """
-    Extract table userid rows by tool invocation counts
-    normalise to total jobs to remove effects of user total volume of analyses
-    User with 10 bwa jobs out of 10 total jobs is a long distance from a user with 10 bwa jobs out of 100000 jobs
+    generic get a connection to postgres
     """
     postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
     password=POSTGRES_PASSWORD,
@@ -52,109 +51,120 @@ def pgjobs(CHUNKSIZE = 1000,
     port=POSTGRES_PORT,
     dbname=POSTGRES_DBNAME))
     cnx = create_engine(postgres_str)
-    squery = '''SELECT user_id, tool_id, COUNT(*) as nruns from job WHERE create_time >= '{}'::timestamp AND create_time < '{}'::timestamp GROUP BY user_id, tool_id  ;'''
-    sql = squery.format(DSTART, DFINISH)
-    log.info('squery=%s' % sql)
+    return cnx
+
+def pg_query(cnx, sql = None, CHUNKSIZE = 1000):
+    """
+    generic run a chunked sql query
+    """
+    log.info('sql=%s' % sql)
     dfs = []
     for chunk in pd.read_sql(sql, con=cnx, chunksize=CHUNKSIZE):
         dfs.append(chunk)
-    jobs = pd.concat(dfs)
+    res = pd.concat(dfs)
+    return res
+
+def mds_query(cnx, DSTART = '2000-01-01 00:00:01',
+                            DFINISH = '2022-06-01 00:00:01'):
+    #forever may be too long on main!!
+    DODENDRO = True
+    # WARNING!! this will take a huge amount of time for a big dataset :-(
+    # twice as long as the mds for the faked 1000x100 data
+    # it's another way to look at the results...
+
+    def fakejobs(NTOOL = 100, NUSERID = 1000, NGROUPS = 5):
+        # synthesise NGROUPS obviously different users
+        # to test mds plot code without real data
+        sjob = []
+        for userid in range(NUSERID):
+            srow = []
+            for toolid in range(NTOOL):
+                srow.append(random.randint(0,1000))
+            group = (userid % NGROUPS)
+            for r in range(group, NTOOL, NGROUPS):
+                srow[r] = 5*srow[r] # fake strong group bias
+            # scale so tool frequencies sum to 1
+            fsum = float(sum(srow))
+            nrow = [x/fsum for x in srow]
+            nrow.insert(0,userid)
+            sjob.append(nrow)
+            job = pd.DataFrame(sjob)
+            job = job.drop(job.columns[[0]], axis=1)
+        return job
+
+    def stresstest(jobs):
+        dist_euclid = euclidean_distances(jobs)
+        stress = []
+        # Max value for n_components
+        max_range = 20
+        for dim in range(1, max_range):
+            mds = MDS(n_components=dim, random_state=0)
+            pts = mds.fit_transform(dist_euclid)
+            stress.append(mds.stress_)
+        # Plot stress vs. n_components
+        plt.plot(range(1, max_range), stress)
+        plt.xticks(range(1, max_range, 2))
+        plt.xlabel('n_components')
+        plt.ylabel('stress')
+        plt.show()
+
+    def heatdendro(dm, dat):
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+        dm = (dm + dm.T) / 2
+        np.fill_diagonal(dm, 0)
+        dist_linkage = hierarchy.ward(squareform(dm))
+        dendro = hierarchy.dendrogram(
+        dist_linkage,  ax=ax1, leaf_rotation=90)
+        dendro_idx = np.arange(0, len(dendro["ivl"]))
+        ax2.imshow(dm[dendro["leaves"], :][:, dendro["leaves"]])
+        ax2.set_xticks(dendro_idx)
+        ax2.set_yticks(dendro_idx)
+        ax2.set_xticklabels(dendro["ivl"], rotation="vertical")
+        ax2.set_yticklabels(dendro["ivl"])
+        fig.tight_layout()
+        plt.savefig('heatdendro.pdf')
+
+    def plotjobs(j):
+        jobs = pd.DataFrame(j)
+        mds = MDS(random_state=0, n_jobs = NCPU)
+        jobs_transform = mds.fit_transform(jobs)
+        size = [5]
+        plt.scatter(jobs_transform[:,0], jobs_transform[:,1], s=size)
+        plt.title('Users in tool usage space')
+        plt.savefig('user_in_toolspace_mds.pdf')
+        # heatmap(mds.dissimilarity_matrix_,j)
+        log.info('stress=%f' % mds.stress_)
+        return mds
+
+    squery = '''SELECT user_id, tool_id, COUNT(*) as nruns from job WHERE create_time >= '{}'::timestamp AND create_time < '{}'::timestamp GROUP BY user_id, tool_id  ;'''
+    sql = squery.format(DSTART, DFINISH)
+    started = time.time()
+    jobs = pg_query(cnx, sql = sql)
+    log.info('Query took %f seconds' % (time.time() - started))
     wjobs = jobs.pivot(index='user_id', columns='tool_id', values='nruns')
     # too hairy to do in SQL !!! Postgres crosstab is horrid - trivial in pandas.
     wjobs = wjobs.fillna(0)
     rjobs = wjobs.div(wjobs.sum(axis=1), axis=0)
     # scale user tool nruns into a fraction of their total work - remove uninteresting total work volume
-    return rjobs
+    mstarted = time.time()
+    nr = len(rjobs)
+    log.info('Retrieving jobs took %f sec and returned %d rows' % (mstarted - started,nr))
+    if nr > 2:
+        mds = plotjobs(rjobs)
+        log.info('MDS with %d CPU took %f sec' % (NCPU, time.time() - mstarted))
+        if DODENDRO:
+            hstarted = time.time()
+            heatdendro(mds.dissimilarity_matrix_, rjobs)
+            log.info('heat/dendro plot took %f sec' % (time.time() - hstarted))
+    else:
+        log.warning('1 or less rows in query result - check that the time interval is sane?')
 
 
-def fakejobs():
-    # synthesise NGROUPS obviously different users
-    # to test mds plot code without real data
-    sjob = []
-    for userid in range(NUSERID):
-        srow = []
-        for toolid in range(NTOOL):
-            srow.append(random.randint(0,1000))
-        group = (userid % NGROUPS)
-        for r in range(group, NTOOL, NGROUPS):
-            srow[r] = 5*srow[r] # fake strong group bias
-        # scale so tool frequencies sum to 1
-        fsum = float(sum(srow))
-        nrow = [x/fsum for x in srow]
-        nrow.insert(0,userid)
-        sjob.append(nrow)
-        job = pd.DataFrame(sjob)
-        job = job.drop(job.columns[[0]], axis=1)
-    return job
-
-
-def stresstest(jobs):
-    dist_euclid = euclidean_distances(jobs)
-    stress = []
-    # Max value for n_components
-    max_range = 20
-    for dim in range(1, max_range):
-        mds = MDS(n_components=dim, random_state=0)
-        pts = mds.fit_transform(dist_euclid)
-        stress.append(mds.stress_)
-    # Plot stress vs. n_components
-    plt.plot(range(1, max_range), stress)
-    plt.xticks(range(1, max_range, 2))
-    plt.xlabel('n_components')
-    plt.ylabel('stress')
-    plt.show()
-
-def heatdendro(dm, dat):
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
-    dm = (dm + dm.T) / 2
-    np.fill_diagonal(dm, 0)
-    dist_linkage = hierarchy.ward(squareform(dm))
-    dendro = hierarchy.dendrogram(
-    dist_linkage,  ax=ax1, leaf_rotation=90)
-    dendro_idx = np.arange(0, len(dendro["ivl"]))
-    ax2.imshow(dm[dendro["leaves"], :][:, dendro["leaves"]])
-    ax2.set_xticks(dendro_idx)
-    ax2.set_yticks(dendro_idx)
-    ax2.set_xticklabels(dendro["ivl"], rotation="vertical")
-    ax2.set_yticklabels(dendro["ivl"])
-    fig.tight_layout()
-    plt.savefig('heatdendro.pdf')
-
-def plotjobs(j):
-    jobs = pd.DataFrame(j)
-    jobarray = euclidean_distances(jobs) # precompute - returns numpy array
-    mds = MDS(random_state=0, dissimilarity="precomputed", n_jobs = NCPU)
-    jobs_transform = mds.fit_transform(jobarray)
-    size = [5]
-    plt.scatter(jobs_transform[:,0], jobs_transform[:,1], s=size)
-    plt.title('Users in tool usage space')
-    plt.savefig('user_in_toolspace_mds.pdf')
-    # heatmap(mds.dissimilarity_matrix_,j)
-    log.info('stress=%f' % mds.stress_)
-    return mds
-
-
-DODENDRO = True
-# WARNING!! this will take a huge amount of time for a big dataset :-(
-# twice as long as the mds for the faked 1000x100 data
-# it's another way to look at the results...
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger()
-started = time.time()
 log.info('plotusermds.py starting %s' % datetime.today())
-# e.g. for a one month test
-# jobs = pgjobs(DSTART = '2022-01-01 00:00:01', DFINISH = '2022-01-31 23:59:59')
-jobs = pgjobs()
-mstarted = time.time()
-nr = len(jobs)
-log.info('Retrieving jobs took %f sec and returned %d rows' % (mstarted - started,nr))
-if nr > 2:
-    mds = plotjobs(jobs)
-    log.info('MDS with %d CPU took %f sec' % (NCPU, time.time() - mstarted))
-    if DODENDRO:
-        hstarted = time.time()
-        heatdendro(mds.dissimilarity_matrix_, jobs)
-        log.info('heat/dendro plot took %f sec' % (time.time() - hstarted))
-else:
-    log.warning('1 or less rows in query result - check that the time interval is sane?')
+cnx = pg_cnx()
+mds_query(cnx, DSTART = '2000-01-01 00:00:01',
+        DFINISH = '2022-06-01 00:00:01')
+        # forever - might be too big to cope with on main!
 log.info('plotusermds.py finished %s' % datetime.today())
